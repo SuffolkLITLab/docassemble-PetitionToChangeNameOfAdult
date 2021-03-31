1345ae83af09bddf94a7c7690e5b40c9a8fdd65a
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-03-30 21:02:47 -0400

    Finishing touches for the download screen

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.PetitionToChangeNameOfAdult',
-      version='2.0',
+      version='2.1',
       description=(''),
       long_description='This is a form for allowing an adult to change their legal name.',
       long_description_content_type='text/markdown',
