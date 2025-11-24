2f29b44e3842dbc49acfdebcaf0f04696a0bec39
Bryce Willey <bryce.willey@suffolk.edu>
2025-11-24 16:58:45 -0500

    Use implicit namespaces over explicit namespaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -51,8 +51,7 @@ setup(name='docassemble.PetitionToChangeNameOfAdult',
       author_email='lkaris@conncoll.edu',
       license='MIT',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=['docassemble.ALAffidavitOfIndigency>=2.0.3', 'docassemble.CARIRequest'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/PetitionToChangeNameOfAdult/', package='docassemble.PetitionToChangeNameOfAdult'),
